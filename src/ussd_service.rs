03f8c3b141ee9c8a93c1492e13bb4e37703e22b2
rhaqim <anusiemj@gmail.com>
2024-03-08 03:25:24 +0100

    code clean up

--- src/ussd_service.rs
+++ src/ussd_service.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 
-use crate::{types::HashStrAny, ussd_session::USSDSession};
+use crate::{prelude::USSDRequest, types::HashStrAny, ussd_session::USSDSession};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct USSDService {
@@ -15,7 +15,7 @@ pub struct USSDService {
 pub trait USSDServiceTrait {
     fn new(functions_path: String, function_name: String, function_url: Option<String>, data_key: String) -> Self;
     fn call(&self, session: &mut USSDSession) -> String;
-    fn load_function(&self) -> Box<dyn Fn(&str) -> HashStrAny>;
+    fn load_function(&self) -> Box<dyn Fn(&USSDRequest, &str) -> HashStrAny>;
 }
 
 impl USSDServiceTrait for USSDService {
@@ -32,12 +32,20 @@ impl USSDServiceTrait for USSDService {
         // Find and load the function from the functions_path
         // Logic to load the function from the function_path (You need to implement this logic)
         let loaded_function = self.load_function();
+
+        let request = USSDRequest {
+            msisdn: session.msisdn.clone(),
+            session_id: session.session_id.clone(),
+            input: "".to_string(),
+            service_code: "".to_string(),
+            language: "".to_string(),
+        };
         
         // Pass the function_url as an argument to the loaded function
         let result = if let Some(url) = &self.function_url {
-            loaded_function(url)
+            loaded_function(&request, url)
         } else {
-            loaded_function("")
+            loaded_function(&request, "")
         };
         
         // Save the returned result in the session data with the data_key
@@ -47,13 +55,13 @@ impl USSDServiceTrait for USSDService {
         format!("{:?}", result)
     }
 
-    fn load_function(&self) -> Box<dyn Fn(&str) -> HashStrAny> {
+    fn load_function(&self) -> Box<dyn Fn(&USSDRequest, &str) -> HashStrAny> {
         // Load the function from the functions_path
         // Logic to load the function from the function_path (You need to implement this logic)
         // This implementation currently returns a function that returns None for all function paths.
         // You need to replace this with your actual implementation.
     
-        Box::new(|_url| {
+        Box::new(|_request, _url| {
             // Creating a sample HashMap for demonstration
             let mut dict = HashMap::new();
             dict.insert("key1".to_string(), HashStrAny::Int(42));
