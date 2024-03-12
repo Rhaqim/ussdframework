ddb22075316eed00772e78020cb3bfe3e4b53d93
Rhaqim <anusiemj@gmail.com>
2024-03-12 09:17:54 +0100

    converting from json to HashStrAny and vice versa

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,7 @@
 use std::{collections::HashMap, fmt::Display};
 
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum RouterOptions {
@@ -25,28 +26,11 @@ impl Display for RouterOptions {
     }
 }
 
-// add to_string method to RouterOptions for String
-// impl RouterOptions {
-//     pub fn to_string(&self) -> String {
-//         match self {
-//             RouterOptions::List(options) => {
-//                 format!("{:?}", options)
-//             }
-//             RouterOptions::ListStr(options) => {
-//                 format!("{:?}", options)
-//             }
-//             RouterOptions::String(option) => {
-//                 option.to_string()
-//             }
-//         }
-//     }
-// }
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub enum HashStrAny {
     Str(String),
-    Int(i32),
-    Float(f32),
+    Int(i64),
+    Float(f64),
     List(Vec<HashStrAny>),
     ListStr(Vec<String>),
     Dict(HashMap<String, HashStrAny>),
@@ -57,6 +41,128 @@ impl HashStrAny {
     // Helper function to create a new HashStrAny::Dict variant
     pub fn new_dict(dict: HashMap<String, HashStrAny>) -> HashStrAny {
         HashStrAny::Dict(dict)
+    }
+}
+
+// Formatter for HashStrAny to convert JSON to HashStrAny
+impl Display for HashStrAny {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            HashStrAny::Str(s) => write!(f, "{}", s),
+            HashStrAny::Int(i) => write!(f, "{}", i),
+            HashStrAny::Float(fl) => write!(f, "{}", fl),
+            HashStrAny::List(l) => write!(f, "{:?}", l),
+            HashStrAny::ListStr(l) => write!(f, "{:?}", l),
+            HashStrAny::Dict(d) => write!(f, "{:?}", d),
+            HashStrAny::None => write!(f, "None"),
+        }
+    }
+}
+
+/// Convert a JSON value to a HashStrAny value
+/// This function is recursive and will convert nested JSON values to HashStrAny values
+/// The HashStrAny type is a custom enum type that can represent any JSON value
+/// 
+/// # Arguments
+/// 
+/// * `json` - A JSON value
+/// 
+/// # Returns
+/// 
+/// A HashStrAny value
+/// 
+/// # Example
+/// 
+/// ```
+/// use serde_json::json;
+/// use serde_json::Value;
+/// 
+/// let json = json!({
+///    "one": "1",
+///   "two": "2"
+/// });
+/// 
+/// let hash_str_any = json_to_hash_str_any(json);
+/// ```
+pub fn json_to_hash_str_any(json: Value) -> HashStrAny {
+    match json {
+        Value::Null => HashStrAny::None,
+        Value::Bool(b) => HashStrAny::Str(b.to_string()),
+        Value::Number(n) => {
+            if let Some(i) = n.as_i64() {
+                HashStrAny::Int(i as i64)
+            } else if let Some(f) = n.as_f64() {
+                HashStrAny::Float(f as f64)
+            } else {
+                HashStrAny::None
+            }
+        }
+        Value::String(s) => HashStrAny::Str(s),
+        Value::Array(arr) => {
+            let mut list = Vec::new();
+            for val in arr {
+                list.push(json_to_hash_str_any(val));
+            }
+            HashStrAny::List(list)
+        }
+        Value::Object(obj) => {
+            let mut dict = HashMap::new();
+            for (key, val) in obj {
+                dict.insert(key, json_to_hash_str_any(val));
+            }
+            HashStrAny::Dict(dict)
+        }
+    }
+}
+
+/// Convert a HashStrAny value to a JSON value
+/// This function is recursive and will convert nested HashStrAny values to JSON values
+/// The HashStrAny type is a custom enum type that can represent any JSON value
+/// 
+/// # Arguments
+/// 
+/// * `hash_any` - A HashStrAny value
+/// 
+/// # Returns
+/// 
+/// A JSON value
+/// 
+/// # Example
+/// 
+/// ```
+/// use serde_json::json;
+/// use serde_json::Value;
+/// 
+/// let hash_str_any = HashStrAny::Dict({
+///    let mut dict = HashMap::new();
+///   dict.insert("one".to_string(), HashStrAny::Str("1".to_string()));
+///  dict.insert("two".to_string(), HashStrAny::Str("2".to_string()));
+/// dict
+/// });
+/// 
+/// let json = hash_str_any_to_json(hash_str_any);
+/// `
+pub fn hash_str_any_to_json(hash_any: HashStrAny) -> Value {
+    match hash_any {
+        HashStrAny::Str(s) => Value::String(s),
+        HashStrAny::Int(i) => Value::Number(i.into()),
+        HashStrAny::Float(f) => Value::Number((f as i64).into()),
+        HashStrAny::List(list) => {
+            let items: Vec<Value> = list
+                .into_iter()
+                .map(|item| hash_str_any_to_json(item))
+                .collect();
+            Value::Array(items)
+        }
+        HashStrAny::ListStr(list) => Value::Array(list.into_iter().map(Value::String).collect()),
+        HashStrAny::Dict(dict) => {
+            let mut obj = serde_json::Map::new();
+            for (key, value) in dict {
+                obj.insert(key, hash_str_any_to_json(value));
+            }
+            Value::Object(obj)
+        }
+        HashStrAny::None => Value::Null,
     }
 }
 
@@ -109,5 +215,80 @@ impl<T> Stack<T> {
     // First item in the stack
     pub fn first(&self) -> Option<&T> {
         self.items.first()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use super::*;
+
+    #[test]
+    fn test_router_options() {
+        let options = RouterOptions::ListStr(vec!["one".to_string(), "two".to_string()]);
+        assert_eq!(options.to_string(), "[\"one\", \"two\"]");
+    }
+
+    #[test]
+    fn test_hash_str_any() {
+        let mut dict = HashMap::new();
+        dict.insert("one".to_string(), HashStrAny::Str("1".to_string()));
+        dict.insert("two".to_string(), HashStrAny::Str("2".to_string()));
+        let hash_str_any = HashStrAny::new_dict(dict);
+        let expected_json = json!({
+            "one": "1",
+            "two": "2"
+        });
+        let json = hash_str_any_to_json(hash_str_any);
+        // Convert both JSON values to strings for comparison
+        let json_str = serde_json::to_string(&json).unwrap();
+        let expected_json_str = serde_json::to_string(&expected_json).unwrap();
+        // assert_eq!(hash_str_any.to_string(), "{\"one\": \"1\", \"two\": \"2\"}");
+        assert_eq!(json_str, expected_json_str);
+    }
+
+    #[test]
+    fn test_json_to_hash_str_any() {
+        let json = json!({
+            "one": "1",
+            "two": "2"
+        });
+
+        let hash_str_any = json_to_hash_str_any(json);
+
+        let mut expected_dict = HashMap::new();
+        expected_dict.insert("one".to_string(), HashStrAny::Str("1".to_string()));
+        expected_dict.insert("two".to_string(), HashStrAny::Str("2".to_string()));
+
+        let expected_hash_str_any = HashStrAny::Dict(expected_dict);
+
+        assert_eq!(hash_str_any, expected_hash_str_any);
+    }
+
+    #[test]
+    fn test_hash_str_any_to_json() {
+        let mut dict = HashMap::new();
+        dict.insert("one".to_string(), HashStrAny::Str("1".to_string()));
+        dict.insert("two".to_string(), HashStrAny::Str("2".to_string()));
+        let hash_str_any = HashStrAny::new_dict(dict);
+        let json = hash_str_any_to_json(hash_str_any);
+        let expected_json = json!({
+            "one": "1",
+            "two": "2"
+        });
+        assert_eq!(json, expected_json);
+    }
+
+    #[test]
+    fn test_stack() {
+        let mut stack = Stack::new();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        assert_eq!(stack.pop(), Some(3));
+        assert_eq!(stack.pop(), Some(2));
+        assert_eq!(stack.pop(), Some(1));
+        assert_eq!(stack.pop(), None);
     }
 }
