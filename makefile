0c7904af8e6d8d04a4cab1d84aef13968a70fc62
Rhaqim <anusiemj@gmail.com>
2024-03-18 02:54:39 +0100

    Taking out the frontend

--- makefile
+++ makefile
@@ -1,2 +1,10 @@
 run-example:
 	cargo run --example basic_usage
+
+build-frontend:
+	cd src/frontend && npm install && npm run build
+
+copy-frontend:
+	cp -r src/frontend/.next/* src/static/
+
+build-copy-frontend: build-frontend copy-frontend
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
+// src/main.rs
 
-mod api;
-mod database;
+use actix_web::{web, App, HttpRequest, HttpServer, Result};
+use actix_files::Files;
+use std::path::PathBuf;
 
-#[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+// async fn index(_req: HttpRequest) -> Result<actix_files::NamedFile> {
+//     Ok(actix_files::NamedFile::open("./static/server/app/index.html")?) // Adjust path as needed
+// }
+
+async fn index(_req: HttpRequest) -> Result<actix_files::NamedFile> {
+    let path: PathBuf = "./_next/server/app/index.html".into(); // Adjust path as needed
+    println!("Attempting to open file: {:?}", path); // Log the resolved file path
+    Ok(actix_files::NamedFile::open(path)?)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(web::resource("/api/data").route(web::get().to(get_data)))
+            .default_service(web::get().to(index)) // Serve the index.html for all other routes
+            .service(Files::new("/", "./_next").index_file("/server/app/index.html")) // Serve all files under the static directory
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+
+
+///  The  get_data  function is a simple function that returns a string.
+///  The  index  function is a simple function that returns a file.
+///  The  main  function is the entry point of the application. It creates a new  HttpServer  and binds it to the address
+async fn get_data() -> Result<String> {
+    Ok("Some data".to_string())
 }
