490f95a3e55e2f34c3d51c8ba67e62899a1fe8f9
Rhaqim <anusiemj@gmail.com>
2024-03-19 06:42:22 +0100

    Comment out

--- src/interface/screen.rs
+++ src/interface/screen.rs
@@ -96,200 +96,200 @@ table! {
     }
 }
 
-impl Database<MenuItem> for DatabaseManager {
-    fn insert(&mut self, menu_item: MenuItem, extra: Option<String>) -> Result<(), Box<dyn Error>> {
-        use self::menu_items::dsl::*;
+// impl Database<MenuItem> for DatabaseManager {
+//     fn insert(&mut self, menu_item: MenuItem, extra: Option<String>) -> Result<(), Box<dyn Error>> {
+//         use self::menu_items::dsl::*;
 
-        let new_menu_item = MenuItem {
-            option: menu_item.option,
-            display_name: menu_item.display_name,
-            next_screen: menu_item.next_screen,
-        };
+//         let new_menu_item = MenuItem {
+//             option: menu_item.option,
+//             display_name: menu_item.display_name,
+//             next_screen: menu_item.next_screen,
+//         };
 
-        diesel::insert_into(menu_items)
-            .values(&new_menu_item)
-            .execute(&mut self.connection)?;
+//         diesel::insert_into(menu_items)
+//             .values(&new_menu_item)
+//             .execute(&mut self.connection)?;
 
-        Ok(())
-    }
+//         Ok(())
+//     }
 
-    fn update(&self, id: i32, menu_item: MenuItem) -> Result<(), Box<dyn Error>> {
-        use self::menu_items::dsl::*;
+//     fn update(&self, id: i32, menu_item: MenuItem) -> Result<(), Box<dyn Error>> {
+//         use self::menu_items::dsl::*;
 
-        let new_menu_item = MenuItem {
-            option: menu_item.option,
-            display_name: menu_item.display_name,
-            next_screen: menu_item.next_screen,
-        };
+//         let new_menu_item = MenuItem {
+//             option: menu_item.option,
+//             display_name: menu_item.display_name,
+//             next_screen: menu_item.next_screen,
+//         };
 
-        diesel::update(menu_items.find(id))
-            .set(new_menu_item)
-            .execute(&mut self.connection)?;
+//         diesel::update(menu_items.find(id))
+//             .set(new_menu_item)
+//             .execute(&mut self.connection)?;
 
-        Ok(())
-    }
+//         Ok(())
+//     }
 
-    fn delete(&self, id: i32) -> Result<(), Box<dyn Error>> {
-        use self::menu_items::dsl::*;
+//     fn delete(&self, id: i32) -> Result<(), Box<dyn Error>> {
+//         use self::menu_items::dsl::*;
 
-        diesel::delete(menu_items.find(id)).execute(&mut self.connection)?;
+//         diesel::delete(menu_items.find(id)).execute(&mut self.connection)?;
 
-        Ok(())
-    }
+//         Ok(())
+//     }
 
-    fn get(&self, id: i32) -> Result<MenuItem, Box<dyn Error>> {
-        use self::menu_items::dsl::*;
+//     fn get(&self, id: i32) -> Result<MenuItem, Box<dyn Error>> {
+//         use self::menu_items::dsl::*;
 
-        let menu_item = menu_items
-            .find(id)
-            .first::<MenuItem>(&mut self.connection)?;
+//         let menu_item = menu_items
+//             .find(id)
+//             .first::<MenuItem>(&mut self.connection)?;
 
-        Ok(menu_item)
-    }
+//         Ok(menu_item)
+//     }
 
-    fn get_all(&self) -> Result<Vec<MenuItem>, Box<dyn Error>> {
-        use self::menu_items::dsl::*;
+//     fn get_all(&self) -> Result<Vec<MenuItem>, Box<dyn Error>> {
+//         use self::menu_items::dsl::*;
 
-        let results = menu_items.load::<MenuItem>(&mut self.connection)?;
+//         let results = menu_items.load::<MenuItem>(&mut self.connection)?;
 
-        Ok(results)
-    }
-}
+//         Ok(results)
+//     }
+// }
 
-impl Database<RouterOption> for DatabaseManager {
-    fn insert(&mut self, routers: RouterOption) -> Result<(), Box<dyn Error>> {
-        use self::router_options::dsl::*;
+// impl Database<RouterOption> for DatabaseManager {
+//     fn insert(&mut self, routers: RouterOption) -> Result<(), Box<dyn Error>> {
+//         use self::router_options::dsl::*;
 
-        let new_router_option = RouterOption {
-            router_option: routers.router_option,
-            next_screen: routers.next_screen,
-        };
+//         let new_router_option = RouterOption {
+//             router_option: routers.router_option,
+//             next_screen: routers.next_screen,
+//         };
 
-        diesel::insert_into(router_options)
-            .values(&new_router_option)
-            .execute(&mut self.connection)?;
+//         diesel::insert_into(router_options)
+//             .values(&new_router_option)
+//             .execute(&mut self.connection)?;
 
-        Ok(())
-    }
+//         Ok(())
+//     }
 
-    fn update(&self, id: i32, routers: RouterOption) -> Result<(), Box<dyn Error>> {
-        use self::router_options::dsl::*;
+//     fn update(&self, id: i32, routers: RouterOption) -> Result<(), Box<dyn Error>> {
+//         use self::router_options::dsl::*;
 
-        let new_router_option = RouterOption {
-            router_option: routers.router_option,
-            next_screen: routers.next_screen,
-        };
+//         let new_router_option = RouterOption {
+//             router_option: routers.router_option,
+//             next_screen: routers.next_screen,
+//         };
 
-        diesel::update(router_options.find(id))
-            .set(new_router_option)
-            .execute(&mut self.connection)?;
+//         diesel::update(router_options.find(id))
+//             .set(new_router_option)
+//             .execute(&mut self.connection)?;
 
-        Ok(())
-    }
+//         Ok(())
+//     }
 
-    fn delete(&self, id: i32) -> Result<(), Box<dyn Error>> {
-        use self::router_options::dsl::*;
+//     fn delete(&self, id: i32) -> Result<(), Box<dyn Error>> {
+//         use self::router_options::dsl::*;
 
-        diesel::delete(router_options.find(id)).execute(&mut self.connection)?;
+//         diesel::delete(router_options.find(id)).execute(&mut self.connection)?;
 
-        Ok(())
-    }
+//         Ok(())
+//     }
 
-    fn get(&self, id: i32) -> Result<RouterOption, Box<dyn Error>> {
-        use self::router_options::dsl::*;
+//     fn get(&self, id: i32) -> Result<RouterOption, Box<dyn Error>> {
+//         use self::router_options::dsl::*;
 
-        let router = router_options
-            .find(id)
-            .first::<RouterOption>(&mut self.connection)?;
+//         let router = router_options
+//             .find(id)
+//             .first::<RouterOption>(&mut self.connection)?;
 
-        Ok(router)
-    }
+//         Ok(router)
+//     }
 
-    fn get_all(&self) -> Result<Vec<RouterOption>, Box<dyn Error>> {
-        use self::router_options::dsl::*;
+//     fn get_all(&self) -> Result<Vec<RouterOption>, Box<dyn Error>> {
+//         use self::router_options::dsl::*;
 
-        let results = router_options.load::<RouterOption>(&mut self.connection)?;
+//         let results = router_options.load::<RouterOption>(&mut self.connection)?;
 
-        Ok(results)
-    }
-}
+//         Ok(results)
+//     }
+// }
 
-impl Database<Screen> for DatabaseManager {
-    fn insert(&mut self, screen: Screen) -> Result<(), Box<dyn Error>> {
-        use self::screens::dsl::*;
+// impl Database<Screen> for DatabaseManager {
+//     fn insert(&mut self, screen: Screen) -> Result<(), Box<dyn Error>> {
+//         use self::screens::dsl::*;
 
-        // insert the menu items and router options first
+//         // insert the menu items and router options first
 
-        if let Some(menu_itemz) = screen.menu_items {
-            for (_, menu_item) in menu_items.iter() {
-                self.insert(menu_item.clone())?;
-            }
-        }
+//         if let Some(menu_itemz) = screen.menu_items {
+//             for (_, menu_item) in menu_items.iter() {
+//                 self.insert(menu_item.clone())?;
+//             }
+//         }
 
-        let new_screen = Screen {
-            text: screen.text,
-            screen_type: screen.screen_type,
-            default_next_screen: screen.default_next_screen,
-            service_code: screen.service_code,
-            menu_items: None,
-            function: screen.function,
-            router_options: None,
-            input_identifier: screen.input_identifier,
-            input_type: screen.input_type,
-        };
+//         let new_screen = Screen {
+//             text: screen.text,
+//             screen_type: screen.screen_type,
+//             default_next_screen: screen.default_next_screen,
+//             service_code: screen.service_code,
+//             menu_items: None,
+//             function: screen.function,
+//             router_options: None,
+//             input_identifier: screen.input_identifier,
+//             input_type: screen.input_type,
+//         };
 
-        diesel::insert_into(screens)
-            .values(&new_screen)
-            .execute(&mut self.connection)?;
+//         diesel::insert_into(screens)
+//             .values(&new_screen)
+//             .execute(&mut self.connection)?;
 
-        Ok(())
-    }
+//         Ok(())
+//     }
 
-    fn update(&self, id: i32, screen: Screen) -> Result<(), Box<dyn Error>> {
-        use self::screens::dsl::*;
+//     fn update(&self, id: i32, screen: Screen) -> Result<(), Box<dyn Error>> {
+//         use self::screens::dsl::*;
 
-        let new_screen = Screen {
-            text: screen.text,
-            screen_type: screen.screen_type,
-            default_next_screen: screen.default_next_screen,
-            service_code: screen.service_code,
-            menu_items: screen.menu_items,
-            function: screen.function,
-            router_options: screen.router_options,
-            input_identifier: screen.input_identifier,
-            input_type: screen.input_type,
-        };
+//         let new_screen = Screen {
+//             text: screen.text,
+//             screen_type: screen.screen_type,
+//             default_next_screen: screen.default_next_screen,
+//             service_code: screen.service_code,
+//             menu_items: screen.menu_items,
+//             function: screen.function,
+//             router_options: screen.router_options,
+//             input_identifier: screen.input_identifier,
+//             input_type: screen.input_type,
+//         };
 
-        diesel::update(screens.find(id))
-            .set(new_screen)
-            .execute(&mut self.connection)?;
+//         diesel::update(screens.find(id))
+//             .set(new_screen)
+//             .execute(&mut self.connection)?;
 
-        Ok(())
-    }
+//         Ok(())
+//     }
 
-    fn delete(&self, id: i32) -> Result<(), Box<dyn Error>> {
-        use self::screens::dsl::*;
+//     fn delete(&self, id: i32) -> Result<(), Box<dyn Error>> {
+//         use self::screens::dsl::*;
 
-        diesel::delete(screens.find(id)).execute(&mut self.connection)?;
+//         diesel::delete(screens.find(id)).execute(&mut self.connection)?;
 
-        Ok(())
-    }
+//         Ok(())
+//     }
 
-    fn get(&self, id: i32) -> Result<Screen, Box<dyn Error>> {
-        use self::screens::dsl::*;
+//     fn get(&self, id: i32) -> Result<Screen, Box<dyn Error>> {
+//         use self::screens::dsl::*;
 
-        let screen = screens
-            .find(id)
-            .first::<Screen>(&mut self.connection)?;
+//         let screen = screens
+//             .find(id)
+//             .first::<Screen>(&mut self.connection)?;
 
-        Ok(screen)
-    }
+//         Ok(screen)
+//     }
 
-    fn get_all(&self) -> Result<Vec<Screen>, Box<dyn Error>> {
-        use self::screens::dsl::*;
+//     fn get_all(&self) -> Result<Vec<Screen>, Box<dyn Error>> {
+//         use self::screens::dsl::*;
 
-        let results = screens.load::<Screen>(&mut self.connection)?;
+//         let results = screens.load::<Screen>(&mut self.connection)?;
 
-        Ok(results)
-    }
-}
+//         Ok(results)
+//     }
+// }
